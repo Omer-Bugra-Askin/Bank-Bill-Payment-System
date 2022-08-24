e36169eaa9552e777fa7d809ba38cd90b0ac32fb
Omer-Bugra-Askin <bugraskin@gmail.com>
2022-08-24 04:28:39 +0300

    All Files
    Tüm dosyalar güncellendi

---
+++ WebProje/Models/Bill.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebProje.Models
+{
+    public class Bill
+    {
+
+        public int BillID { get; set; }
+        [Required]
+        public string BillDesc { get; set; }
+        [Required]
+        public float BillAmount { get; set; }
+        [Required]
+        public bool BillStatus { get; set; }
+        [Required]
+        public DateTime BillDate { get; set; }
+        
+        public int CompanyID { get; set; }
+        public virtual Company Company { get; set; }
+    
+        public string UsersId { get; set; } //Foreing key 
+        public virtual Users Users { get; set; } // Bire  çok bağlantı için 
+        [NotMapped]
+        public virtual int BankAccountID { get; set; } 
+
+    
+    }
+}
---
+++ WebProje/Models/Users.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebProje.Models
+{
+    public class Users:IdentityUser
+    {
+        private RoleManager<IdentityRole> roleManager;
+        public string Name { get; set; }
+        public string LastName { get; set; }
+
+        public string    Phone { get; set; }
+        public virtual ICollection<Bill> Bills { get; set; } // Fatura 1 userın birden çok faturası olabilir
+        public  virtual ICollection<BankAccount> BankAccounts { get; set; } //Fatura 1 userın birden çok faturası olabilir
+   
+       // public  virtual ICollection<UserBill> UserBills { get; set; }
+    }
+}
